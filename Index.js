41abc42fd3a53d74ae673b96ead29dfe021a7242
Ahmed-eldaker <ahmeddaker01@gmail.com>
2024-10-29 18:15:46 +0300

    day 3

--- Index.js
+++ Index.js
@@ -1,12 +1,12 @@
-console.log(1 + 2);
-console.log("hello");
-// module
-// exports
-// __dirname
-// __filename
-// console.log(__filename);
-// console.log(__dirname);
-let x = 5;
-module.exports = {
-  x: x,
-};
+// console.log(1 + 2);
+// console.log("hello");
+// // module
+// // exports
+// // __dirname
+// // __filename
+// // console.log(__filename);
+// // console.log(__dirname);
+// let x = 5;
+// module.exports = {
+//   x: x,
+// };
--- Main.js
+++ Main.js
@@ -24,19 +24,59 @@
 //   if (err) throw err;
 //   console.log("created!");
 // });
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.method == "GET" && req.url == "/") {
-    res.write("hello world from node.js");
-  } else if (req.method == "GET" && req.url == "/home") {
-    res.write("method supported for home");
-  } else {
-    res.write("method not supported");
-  }
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   if (req.method == "GET" && req.url == "/home") {
+//     res.write("hello world from node.js");
+//   } else if (req.method == "GET" && req.url == "/home") {
+//     res.write("method supported for home");
+//   } else {
+//     res.write("method not supported");
+//   }
+//   res.end();
+// });
 
-  res.end();
+const express = require("express");
+const app = express();
+// const port = 3000;
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+
+app.get("/users", (req, res) => {
+  res.send("hello world from node.js");
+});
+app.get("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  res.send(`user id is : ${userId}`);
+});
+data = { userName: "ahmed", password: 123456 };
+
+const AJV = require("ajv");
+const ajv = new AJV();
+const schema = {
+  type: "object",
+  properties: {
+    name: { type: "string" },
+    age: { type: "integer", minimum: 18 },
+  },
+  required: ["name", "age"],
+};
+
+app.post("/login", (req, res) => {
+  const validate = ajv.compile(schema);
+  const valid = validate(req.body);
+  if (!valid) {
+    return res.status(400).json({ errors: validate.errors });
+  }
+  const { name, age } = req.body;
+  res.send(`name=${name}  ,  age =${age}`);
 });
 
-server.listen(3000, () => {
-  console.log("server is listening on port 3000...");
+// localhost:3000/users/5
+// app.get('/user')
+// app.post()
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}...`);
 });
