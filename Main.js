8d399f777d77a68696093e960a302ea5d4ea04cb
Ahmed-eldaker <ahmeddaker01@gmail.com>
2024-11-02 03:02:30 +0200

    updating

--- Main.js
+++ Main.js
@@ -1,45 +1,7 @@
-// let x = require("./Index.js");
-// console.log(x);
-// os
-// const os = require("os");
-// console.log(os.hostname());
-// const path = require("path");
-// console.log(path.basename("/test/file.txt"));
-// console.log(path.parse(__dirname,));
-// const fs = require("fs");
-
-// fs.readFile("test.txt", "utf-8", (err, data) => {
-//   if (err) throw console.log(err);
-//   console.log(data);
-// });
-// fs.writeFile("output.txt", "from node file", (err) => {
-//   if (err) throw err;
-//   console.log("file saved");
-// });
-// fs.unlink("output.txt", (err) => {
-//   if (err) throw err;
-//   console.log("deleted!");
-// });
-// fs.mkdir("newFolder", (err) => {
-//   if (err) throw err;
-//   console.log("created!");
-// });
-// const http = require("http");
-// const server = http.createServer((req, res) => {
-//   if (req.method == "GET" && req.url == "/home") {
-//     res.write("hello world from node.js");
-//   } else if (req.method == "GET" && req.url == "/home") {
-//     res.write("method supported for home");
-//   } else {
-//     res.write("method not supported");
-//   }
-//   res.end();
-// });
-
 const express = require("express");
 const app = express();
-// const port = 3000;
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.use(express.json());
 require("dotenv").config();
 const port = process.env.PORT || 4000;
@@ -63,8 +25,16 @@ const schema = {
   },
   required: ["name", "age"],
 };
+const authMiddleware = (req, res, next) => {
+  console.log("waiting....");
 
-app.post("/login", (req, res) => {
+  // res.send("waiting....");
+  next();
+};
+
+app.post("/login", authMiddleware, (req, res) => {
+  console.log("accepted");
+
   const validate = ajv.compile(schema);
   const valid = validate(req.body);
   if (!valid) {
@@ -74,9 +44,11 @@ app.post("/login", (req, res) => {
   res.send(`name=${name}  ,  age =${age}`);
 });
 
-// localhost:3000/users/5
-// app.get('/user')
-// app.post()
+app.get("/setCookie", (req, res) => {
+  res.cookie("name", "Ahmed", { maxAge: 30000, httpOnly: true });
+  res.send("cookie sent");
+});
+
 app.listen(port, () => {
   console.log(`server is listening on port ${port}...`);
 });
